b77b21038bb8411e9d86e510521a1d2254708e29
QuantumCores <kanonear@gmail.com>
2022-08-12 20:13:06 +0200

    QC: IS4

---
+++ Challenger.Identity/Startup.cs
@@ -0,0 +1,108 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using Challenger.Identity.Migrations.IdentityServer.IdentityDb;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+
+namespace Challenger.Identity
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(
+            IWebHostEnvironment environment,
+            IConfiguration configuration)
+        {
+            Environment = environment;
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddDbContext<IdentityContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()//options => options.SignIn.RequireConfirmedAccount = false)
+                            .AddEntityFrameworkStores<IdentityContext>()
+                            .AddDefaultTokenProviders();
+
+            var migrationAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            var builder = services.AddIdentityServer(options =>
+            {
+                options.Events.RaiseErrorEvents = true;
+                options.Events.RaiseInformationEvents = true;
+                options.Events.RaiseFailureEvents = true;
+                options.Events.RaiseSuccessEvents = true;
+
+                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+                options.EmitStaticAudienceClaim = true;
+            })
+            .AddConfigurationStore(opt =>
+            {
+                opt.ConfigureDbContext = c => c.UseSqlServer(Configuration.GetConnectionString("IdentityConfigurationConnection"),
+                    sql => sql.MigrationsAssembly(migrationAssembly));
+            })
+            .AddOperationalStore(opt =>
+            {
+                opt.ConfigureDbContext = o => o.UseSqlServer(Configuration.GetConnectionString("IdentityConfigurationConnection"),
+                    sql => sql.MigrationsAssembly(migrationAssembly));
+            })
+            .AddAspNetIdentity<IdentityUser>();
+            //.AddTestUsers(TestUsers.Users);
+
+            if (!Environment.IsDevelopment())
+            {
+                builder.AddSigningCredential("someName"); //configure for production!
+            }
+            else
+            {
+                builder.AddDeveloperSigningCredential();
+            }            
+
+            //builder.Services.AddCors(options =>
+            //{
+            //    options.AddPolicy(name: "AllowAnyOrigin",
+            //                      builder =>
+            //                      {
+            //                          builder//.WithOrigins("http://54.37.137.86", "https://54.37.137.86", "http://localhost:4200")
+            //                          .AllowAnyOrigin()
+            //                          .AllowAnyMethod()
+            //                          .AllowAnyHeader();
+            //                          //.WithHeaders("content-type")
+            //                          //.AllowCredentials();
+            //                      });
+            //});
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // uncomment if you want to add MVC
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseIdentityServer();            
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
